13f1ca5b7b1e969fe060a52126bae0a0bfdd6961
ujwal284 <ujwalkhanal284@gmail.com>
2024-10-04 11:44:40 +0545

    created chart.js

---
+++ chartjs/src/App.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import "./App.css";
+import { Chart as ChartJS, defaults } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+function App() {
+  return <></>;
+}
+
+export default App;
